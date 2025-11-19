eb408d35854dbb1b2272b1c4c4c419cd7db67e0f
Conor12345 <conornichols9@gmail.com>
2025-11-19 21:15:13 +0000

    Day 12 part 1

---
+++ day_12/day_12.cpp
@@ -0,0 +1,269 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+
+struct Location
+{
+    int row, col;
+
+    bool operator==(const Location &other) const
+    {
+        if (this->row == other.row && this->col == other.col)
+        {
+            return true;
+        }
+        return false;
+    }
+};
+
+bool location_sorter(Location lhs, Location rhs)
+{
+    if (lhs.row != rhs.row)
+    {
+        return lhs.row < rhs.row;
+    }
+    return lhs.col < rhs.col;
+}
+
+struct MapLocation
+{
+    char item;
+    bool isCounted;
+};
+
+class FenceCalculator
+{
+public:
+    void readMapFromFile(std::string);
+    void logMap();
+    void logCountedMap();
+    int calculateTotalFenceCost();
+
+private:
+    std::vector<Location> findContiguousLocations(int, int);
+    std::vector<Location> findContiguousNeighbours(int, int);
+    bool isNeighbourValid(MapLocation, int, int);
+    void logLocationList(std::vector<Location>);
+    int calculateFenceAreaCost(std::vector<Location>);
+    int countAdjacent(Location, std::vector<Location>);
+
+    std::vector<std::vector<MapLocation>> map;
+};
+
+void FenceCalculator::readMapFromFile(std::string filename)
+{
+    std::string line_in_buffer;
+    std::ifstream File(filename);
+
+    std::vector<MapLocation> line_buffer;
+    std::vector<std::vector<MapLocation>> map_buffer;
+
+    while (std::getline(File, line_in_buffer))
+    {
+        std::vector<char> map_line(line_in_buffer.begin(), line_in_buffer.end());
+        line_buffer.clear();
+        for (char item : map_line)
+        {
+            line_buffer.push_back({item, false});
+        }
+
+        map_buffer.push_back(line_buffer);
+    }
+
+    File.close();
+
+    map = map_buffer;
+}
+
+void FenceCalculator::logMap()
+{
+    for (auto line : map)
+    {
+        for (MapLocation item : line)
+        {
+            std::cout << item.item;
+        }
+        std::cout << "\n";
+    }
+    std::cout << "\n";
+}
+
+void FenceCalculator::logCountedMap()
+{
+    for (auto line : map)
+    {
+        for (MapLocation item : line)
+        {
+            std::cout << item.isCounted;
+        }
+        std::cout << "\n";
+    }
+    std::cout << "\n";
+}
+
+void FenceCalculator::logLocationList(std::vector<Location> list)
+{
+    std::cout << "ROW\tCOL\n";
+    for (auto item : list)
+    {
+        std::cout << item.row << "\t" << item.col << "\n";
+    }
+    std::cout << "\n";
+}
+
+bool FenceCalculator::isNeighbourValid(MapLocation current, int row, int col)
+{
+    if (row < 0)
+    {
+        return false;
+    }
+    if (row >= map.size())
+    {
+        return false;
+    }
+    if (col < 0)
+    {
+        return false;
+    }
+    if (col >= map.at(row).size())
+    {
+        return false;
+    }
+    if (map.at(row).at(col).isCounted)
+    {
+        return false;
+    }
+
+    return map.at(row).at(col).item == current.item;
+}
+
+std::vector<Location> FenceCalculator::findContiguousNeighbours(int row, int col)
+{
+    std::vector<Location> contiguous_locations;
+    MapLocation current = map.at(row).at(col);
+
+    if (isNeighbourValid(current, row - 1, col))
+    {
+        contiguous_locations.push_back({row - 1, col});
+    }
+    if (isNeighbourValid(current, row + 1, col))
+    {
+        contiguous_locations.push_back({row + 1, col});
+    }
+    if (isNeighbourValid(current, row, col - 1))
+    {
+        contiguous_locations.push_back({row, col - 1});
+    }
+    if (isNeighbourValid(current, row, col + 1))
+    {
+        contiguous_locations.push_back({row, col + 1});
+    }
+    return contiguous_locations;
+}
+
+std::vector<Location> FenceCalculator::findContiguousLocations(int row, int col)
+{
+    map.at(row).at(col).isCounted = true;
+
+    std::vector<Location> contiguous_locations = {{row, col}};
+
+    std::vector<Location> contiguous_neighbours = findContiguousNeighbours(row, col);
+
+    if (contiguous_neighbours.size() == 0)
+    {
+        return {{row, col}};
+    }
+
+    for (Location location : contiguous_neighbours)
+    {
+        contiguous_locations.push_back(location);
+        map.at(location.row).at(location.col).isCounted = true;
+        std::vector<Location> new_contiguous_neighbours = findContiguousLocations(location.row, location.col);
+        for (Location new_location : new_contiguous_neighbours)
+        {
+            map.at(new_location.row).at(new_location.col).isCounted = true;
+            contiguous_locations.push_back(new_location);
+        }
+    }
+    return contiguous_locations;
+}
+
+int FenceCalculator::countAdjacent(Location item, std::vector<Location> locations)
+{
+    int count = 0;
+    for (Location compare : locations)
+    {
+        if (item.row == compare.row + 1 && item.col == compare.col)
+        {
+            count++;
+        }
+        else if (item.row == compare.row - 1 && item.col == compare.col)
+        {
+            count++;
+        }
+        else if (item.row == compare.row && item.col == compare.col + 1)
+        {
+            count++;
+        }
+        else if (item.row == compare.row && item.col == compare.col - 1)
+        {
+            count++;
+        }
+
+        if (count == 4)
+        {
+            return count;
+        }
+    }
+    return count;
+}
+
+int FenceCalculator::calculateFenceAreaCost(std::vector<Location> locations)
+{
+    std::sort(locations.begin(), locations.end(), &location_sorter);
+    std::vector<Location>::iterator ip = std::unique(locations.begin(), locations.end());
+    locations.resize(std::distance(locations.begin(), ip));
+    // logLocationList(locations);
+
+    int area = locations.size();
+    int perimeter = 0;
+    for (Location location : locations)
+    {
+        perimeter += 4 - countAdjacent(location, locations);
+    }
+
+    std::cout << area << "\t" << perimeter << "\n";
+
+    return area * perimeter;
+}
+
+int FenceCalculator::calculateTotalFenceCost()
+{
+    int cost = 0;
+    for (int row = 0; row < map.size(); row++)
+    {
+        for (int col = 0; col < map.at(0).size(); col++)
+        {
+            if (map.at(row).at(col).isCounted)
+            {
+                continue;
+            }
+            std::vector<Location> contiguous_locations = findContiguousLocations(row, col);
+            std::cout << map.at(row).at(col).item << " AT " << row << ", " << col << "\n";
+            // logLocationList(contiguous_locations);
+            cost += calculateFenceAreaCost(contiguous_locations);
+            std::cout << "ROLLING COST: " << cost << "\n\n\n";
+        }
+    }
+    return cost;
+}
+
+int main()
+{
+    FenceCalculator fence_calculator;
+    fence_calculator.readMapFromFile("full_input.txt");
+    fence_calculator.logMap();
+    std::cout << "COST: " << fence_calculator.calculateTotalFenceCost() << "\n";
+}
